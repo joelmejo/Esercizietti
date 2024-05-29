8765193ebd7c5355b198498b94be7f7b1c052326
joelmejo <167082021+joelmejo@users.noreply.github.com>
2024-05-29 12:38:46 +0200

    New folder LeetCode

---
+++ LeetCode/1404.py
@@ -0,0 +1,21 @@
+def numSteps(s: str) -> int:
+        lista: list = list(s)
+        steps: int = 0
+        for char in lista:
+            char = int(char)
+        lista = lista.reverse()
+        for i in len(lista) -1:
+            lista[i] = lista[i] * (2**i)
+        
+        decimal: int= sum(lista)
+
+        while decimal != 1:
+            if decimal % 2 != 0:
+                decimal += 1
+                steps += 1
+            else:
+                decimal //= 2
+                steps += 1
+        return steps
+
+numSteps("1101")
